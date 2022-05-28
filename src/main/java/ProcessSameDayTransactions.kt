035b0e125524ffc110b093c8c276b79cc1983328
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-05-28 16:29:11 +0100

    Improved abstraction and inheritance

--- src/main/java/ProcessSameDayTransactions.kt
+++ src/main/java/ProcessSameDayTransactions.kt
@@ -1,27 +1,20 @@
+
 import model.OutstandingTransactions
 import model.Transaction
-import java.math.BigDecimal
-import java.math.RoundingMode
 
-class ProcessSameDayTransactions : ProcessTransaction {
-
-    private lateinit var outstandingTransactions: OutstandingTransactions
+class ProcessSameDayTransactions(outstandingTransactions: OutstandingTransactions) : ProcessTransaction(outstandingTransactions) {
 
     /**
      * Match all same day asset purchases and disposals.
      * Same day disposals have top priority when matching transactions for an HMRC
      * Capital Gains report.
      *
-     * @param outstandingTransactions An OutstandingTransactions object containing the total profit,
-     * total loss, and outstanding buy and sell transactions when the class is initialised
      * @return An OutstandingTransactions object detailing the total profit, total loss,
      * and lists of outstanding Buy and Sell Transaction objects after all same day
      * transactions have been processed.
      */
     override
-    fun process(outstandingTransactions: OutstandingTransactions): OutstandingTransactions {
-        this.outstandingTransactions = outstandingTransactions
-
+    fun process(): OutstandingTransactions {
         for (sellTransaction in outstandingTransactions.sellTransactions) {
             val buyTransaction = outstandingTransactions.buyTransactions.find {
                 it.date == sellTransaction.date
@@ -39,16 +32,7 @@ class ProcessSameDayTransactions : ProcessTransaction {
      * @param sellTransaction The Transaction object associated with the sales for a given day
      */
     private fun reportSameDayTransaction(buyTransaction: Transaction, sellTransaction: Transaction) {
-        val profitOrLoss = calculateProfitLoss(buyTransaction, sellTransaction)
-        val profitOrLossMessage: String
-        if (profitOrLoss >= 0) {
-            profitOrLossMessage = "Profit = £$profitOrLoss."
-            outstandingTransactions.totalProfit += profitOrLoss
-        } else {
-            profitOrLossMessage = "Loss = £$profitOrLoss."
-            outstandingTransactions.totalLoss += profitOrLoss
-        }
-
+        val profitOrLossMessage = super.getProfitOrLoss(buyTransaction, sellTransaction)
         val averageSellPrice = sellTransaction.price / sellTransaction.quantity
         val averageBuyPrice = buyTransaction.price / buyTransaction.quantity
         val output = "SAME DAY Sell transaction(s) (IDs " + sellTransaction.transactionIDs +
@@ -58,51 +42,5 @@ class ProcessSameDayTransactions : ProcessTransaction {
                 sellTransaction.quantity + " shares were sold for an average price of $averageSellPrice " +
                 "GBP. $profitOrLossMessage"
         println(output)
-    }
-
-    /**
-     * Calculate the profit or loss incurred from the sale of an asset due to the same day disposal
-     * rule.
-     *
-     * @param buyTransaction The Transaction object associated with the purchases for a given day
-     * @param sellTransaction The Transaction object associated with the sales for a given day
-     * @return A Double representing the profit or loss incurred from the transactions
-     */
-    private fun calculateProfitLoss(buyTransaction: Transaction, sellTransaction: Transaction): Double {
-        val profitOrLoss: Double
-        when {
-            // Quantity purchased and sold are equal
-            sellTransaction.quantity == buyTransaction.quantity -> {
-                profitOrLoss = BigDecimal(sellTransaction.price -
-                        buyTransaction.price).setScale(2, RoundingMode.HALF_EVEN).toDouble()
-                outstandingTransactions.buyTransactions.remove(buyTransaction)
-                outstandingTransactions.sellTransactions.remove(sellTransaction)
-            }
-            // Quantity sold is greater than the quantity purchased
-            sellTransaction.quantity > buyTransaction.quantity -> {
-                val percentageOfSoldSharesRemaining = buyTransaction.quantity.toDouble() /
-                        sellTransaction.quantity.toDouble()
-                val valueOfSoldShares = sellTransaction.price * percentageOfSoldSharesRemaining
-                val index = outstandingTransactions.sellTransactions.indexOf(sellTransaction)
-                outstandingTransactions.sellTransactions[index].quantity -= buyTransaction.quantity
-                outstandingTransactions.sellTransactions[index].price -= valueOfSoldShares
-                outstandingTransactions.buyTransactions.remove(buyTransaction)
-                profitOrLoss = BigDecimal(valueOfSoldShares -
-                        buyTransaction.price).setScale(2, RoundingMode.HALF_EVEN).toDouble()
-            }
-            // Quantity purchased is greater than the quantity sold
-            else -> {
-                val percentageOfPurchasedSharesRemaining = sellTransaction.quantity.toDouble() /
-                        buyTransaction.quantity.toDouble()
-                val valueOfPurchasedShares = buyTransaction.price * percentageOfPurchasedSharesRemaining
-                val index = outstandingTransactions.buyTransactions.indexOf(buyTransaction)
-                outstandingTransactions.buyTransactions[index].quantity -= sellTransaction.quantity
-                outstandingTransactions.buyTransactions[index].price -= valueOfPurchasedShares
-                outstandingTransactions.sellTransactions.remove(sellTransaction)
-                profitOrLoss = BigDecimal(sellTransaction.price -
-                        valueOfPurchasedShares).setScale(2, RoundingMode.HALF_EVEN).toDouble()
-            }
-        }
-        return profitOrLoss
     }
 }
--- src/main/java/ProcessTransaction.kt
+++ src/main/java/ProcessTransaction.kt
@@ -1,14 +1,113 @@
 import model.OutstandingTransactions
+import model.Transaction
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-interface ProcessTransaction {
+abstract class ProcessTransaction(outstandingTransactions: OutstandingTransactions) {
+
+    val outstandingTransactions: OutstandingTransactions
+
+    init {
+        this.outstandingTransactions = outstandingTransactions
+    }
+
     /**
      * Process a given transaction.
      *
-     * @param outstandingTransactions An OutstandingTransactions object containing the total profit,
-     * total loss, and outstanding buy and sell transactions when the class is initialised
      * @return An OutstandingTransactions object detailing the total profit, total loss,
      * and lists of outstanding Buy and Sell Transaction objects after all transactions managed by the
      * class have been processed.
      */
-    fun process(outstandingTransactions: OutstandingTransactions) : OutstandingTransactions
+    abstract fun process() : OutstandingTransactions
+
+    /**
+     * Returns a summary of the profit or loss resulting from a given transaction.
+     *
+     * @param buyTransaction The Transaction object associated with the purchases for a given day
+     * @param sellTransaction The Transaction object associated with the sales for a given day
+     * @return A String describing the profit or loss incurred from the transactions
+     */
+    fun getProfitOrLoss(buyTransaction: Transaction, sellTransaction: Transaction): String {
+        val profitOrLoss = calculateProfitLoss(buyTransaction, sellTransaction)
+        return if (profitOrLoss >= 0) {
+            outstandingTransactions.totalProfit += profitOrLoss
+            "Profit = £$profitOrLoss."
+        } else {
+            outstandingTransactions.totalLoss += profitOrLoss
+            "Loss = £$profitOrLoss."
+        }
+    }
+
+    /**
+     * Calculate the profit or loss incurred from the sale of an asset.
+     *
+     * @param buyTransaction The Transaction object associated with the purchases for a given day
+     * @param sellTransaction The Transaction object associated with the sales for a given day
+     * @return A Double representing the profit or loss incurred from the transactions
+     */
+    private fun calculateProfitLoss(buyTransaction: Transaction, sellTransaction: Transaction): Double {
+        val profitOrLoss: Double
+        when {
+            // Quantity purchased and sold are equal
+            sellTransaction.quantity == buyTransaction.quantity -> {
+                profitOrLoss = BigDecimal(sellTransaction.price -
+                        buyTransaction.price).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+            }
+            // Quantity sold is greater than the quantity purchased
+            sellTransaction.quantity > buyTransaction.quantity -> {
+                val percentageOfSoldSharesRemaining = buyTransaction.quantity.toDouble() /
+                        sellTransaction.quantity.toDouble()
+                val valueOfSoldShares = sellTransaction.price * percentageOfSoldSharesRemaining
+                profitOrLoss = BigDecimal(valueOfSoldShares -
+                        buyTransaction.price).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+            }
+            // Quantity purchased is greater than the quantity sold
+            else -> {
+                val percentageOfPurchasedSharesRemaining = sellTransaction.quantity.toDouble() /
+                        buyTransaction.quantity.toDouble()
+                val valueOfPurchasedShares = buyTransaction.price * percentageOfPurchasedSharesRemaining
+                profitOrLoss = BigDecimal(sellTransaction.price -
+                        valueOfPurchasedShares).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+            }
+        }
+        updateOutstandingTransactions(buyTransaction, sellTransaction)
+        return profitOrLoss
+    }
+
+    /**
+     * Update the lists of outstanding transactions to reflect the purchases and disposals
+     * that have been matched and no longer require processing.
+     *
+     * @param buyTransaction The Transaction object associated with the purchases for a given day
+     * @param sellTransaction The Transaction object associated with the sales for a given day
+     */
+    private fun updateOutstandingTransactions(buyTransaction: Transaction, sellTransaction: Transaction) {
+        when {
+            // Quantity purchased and sold are equal
+            sellTransaction.quantity == buyTransaction.quantity -> {
+                outstandingTransactions.buyTransactions.remove(buyTransaction)
+                outstandingTransactions.sellTransactions.remove(sellTransaction)
+            }
+            // Quantity sold is greater than the quantity purchased
+            sellTransaction.quantity > buyTransaction.quantity -> {
+                val percentageOfSoldSharesRemaining = buyTransaction.quantity.toDouble() /
+                        sellTransaction.quantity.toDouble()
+                val valueOfSoldShares = sellTransaction.price * percentageOfSoldSharesRemaining
+                val index = outstandingTransactions.sellTransactions.indexOf(sellTransaction)
+                outstandingTransactions.sellTransactions[index].quantity -= buyTransaction.quantity
+                outstandingTransactions.sellTransactions[index].price -= valueOfSoldShares
+                outstandingTransactions.buyTransactions.remove(buyTransaction)
+            }
+            // Quantity purchased is greater than the quantity sold
+            else -> {
+                val percentageOfPurchasedSharesRemaining = sellTransaction.quantity.toDouble() /
+                        buyTransaction.quantity.toDouble()
+                val valueOfPurchasedShares = buyTransaction.price * percentageOfPurchasedSharesRemaining
+                val index = outstandingTransactions.buyTransactions.indexOf(buyTransaction)
+                outstandingTransactions.buyTransactions[index].quantity -= sellTransaction.quantity
+                outstandingTransactions.buyTransactions[index].price -= valueOfPurchasedShares
+                outstandingTransactions.sellTransactions.remove(sellTransaction)
+            }
+        }
+    }
 }
