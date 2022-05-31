b6e1bdaaf594a612ee36b602a86eafcc0bb88ee4
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-05-31 12:16:10 +0100

    Improved the report summary format

--- src/main/java/ProcessTransactions.kt
+++ src/main/java/ProcessTransactions.kt
@@ -1,5 +1,8 @@
+
 import model.OutstandingTransactions
 import model.Transaction
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class ProcessTransactions(var buyTransactions: List<Transaction>, var sellTransactions: List<Transaction>) {
 
@@ -14,5 +17,29 @@ class ProcessTransactions(var buyTransactions: List<Transaction>, var sellTransa
 
         outstandingTransactions = ProcessSameDayTransactions(outstandingTransactions).process()
         outstandingTransactions = ProcessBedAndBreakfastTransactions(outstandingTransactions).process()
+        outstandingTransactions = ProcessSection104Transactions(outstandingTransactions).process()
+        outstandingTransactions = ProcessAcquisitionFollowingDisposalTransactions(outstandingTransactions).process()
+        printReportSummary(outstandingTransactions)
+    }
+
+    // TODO - TEST ME
+    private fun printReportSummary(outstandingTransactions: OutstandingTransactions) {
+        println()
+        if (outstandingTransactions.sellTransactions.isNotEmpty() || outstandingTransactions.buyTransactions.isNotEmpty()) {
+            println("WARNING: The following transactions could not be matched. This " +
+                    "may be because transactions are missing from the input spreadsheet or " +
+                    "the transactions may be matched with disposals/acquisitions in other tax years.")
+            if (outstandingTransactions.sellTransactions.isNotEmpty()) println(outstandingTransactions.sellTransactions)
+            if (outstandingTransactions.buyTransactions.isNotEmpty()) println(outstandingTransactions.buyTransactions)
+            println()
+        }
+
+        val gains = BigDecimal(outstandingTransactions.totalProfit).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+        val losses = BigDecimal(outstandingTransactions.totalLoss).setScale(2, RoundingMode.HALF_EVEN).toDouble()
+        val profits = BigDecimal(gains - losses).setScale(2, RoundingMode.HALF_EVEN).toString()
+        println("Total gains (excluding fees): £$gains \n" +
+                "Total losses (excluding fees): £$losses \n" +
+                "Profit (gains minus losses): £$profits \n" +
+                "Processing complete")
     }
 }
